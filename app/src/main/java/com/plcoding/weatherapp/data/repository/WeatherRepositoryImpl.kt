720d22c3d9b5d6f89635eb9a1bf32b58a9a11270
nick <wk@kucharski.ovh>
2025-09-06 18:43:46 +0200

    Initial Repositories commit

---
+++ app/src/main/java/com/plcoding/weatherapp/data/repository/WeatherRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package com.plcoding.weatherapp.data.repository
+
+import com.plcoding.weatherapp.data.remote.WeatherApi
+import com.plcoding.weatherapp.domain.repository.WeatherRepository
+import com.plcoding.weatherapp.domain.util.Resource
+import com.plcoding.weatherapp.domain.weather.WeatherInfo
+import javax.inject.Inject
+
+class WeatherRepositoryImpl @Inject constructor(
+    private val api: WeatherApi
+): WeatherRepository {
+
+    override suspend fun getWeatherData(lat: Double, long: Double): Resource<WeatherInfo> {
+
+    }
+}
